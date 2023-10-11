ec5a53c2c99cf65305bc85e36c9c887ae63d1007
Bilal Akkuş <Bilalakkus65@hotmail.com>
2023-10-11 15:57:36 +0300

    menuEklendi

--- Admin.project/Views/Shared/Components/MenuListVC/MenuListVC.cs
+++ Admin.project/Views/Shared/Components/MenuListVC/MenuListVC.cs
@@ -1,4 +1,5 @@
 ﻿using Admin.project.Contexts;
+using Admin.project.Models;
 using Admin.project.Views.VM;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,15 +20,30 @@ namespace Admin.project.Views.Shared.Components.MenuListVC
         {
 
             var menus = _context.Menus.ToList();
-            var result = from m in menus
-                         select new MenusVM
-                         {
-                             Icon = m.Icon,
-                             Id = m.Id,
-                             ParentId = m.ParentId,
-                             Url = m.Url
-                         };
-            return View(result);
+            var menu = GetSubmenu(menus, 0);
+            //var result = from m in menus
+            //             select new MenusVM
+            //             {
+            //                 Name=m.Name,
+            //                 Icon = m.Icon,
+            //                 Id = m.Id,
+            //                 ParentId = m.ParentId,
+            //                 Url = m.Url
+            //             };
+            return View(menu);
+        }
+        public List<MenusVM> GetSubmenu(List<Menu> menus, int parentId)
+        {
+            return menus.Where(m => m.ParentId == parentId)
+                .Select(m => new MenusVM
+                {
+                    ParentId = m.ParentId,
+                    Icon = m.Icon,
+                    Id = m.Id,
+                    Name = m.Name,
+                    Url = m.Url,
+                    SubMenuList = GetSubmenu(menus, m.Id)
+                }).ToList();
         }
     }
 }
--- Admin.project/Views/VM/MenusVM.cs
+++ Admin.project/Views/VM/MenusVM.cs
@@ -3,8 +3,10 @@
     public class MenusVM
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public int ParentId { get; set; }
         public string Icon { get; set; }
         public string Url { get; set; }
+        public List<MenusVM> SubMenuList { get; set; }
     }
 }
